2716540e627583e6054fd2f9517e46b7f92958e7
Guoxing Yao <guoxing.yao@ericsson.com>
2018-06-30 15:18:20 +0800

    chapter 4 REST APIS of users.

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
 var logger = require('morgan');
+var fs = require('file-system');
 
 
 var app = express();
 
-// Require file system module
-var fs = require('file-system');
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/express_app', {
+	// useMongoClient: true
+});
+
+    // .catch(err => {
+    //     console.error('App starting error:', err.stack);
+    //     process.exit(1);
+    // });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+    console.log("Connection Succeeded");
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing our favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Include controllers
 fs.readdirSync('controllers').forEach(function (file) {
@@ -17,18 +47,6 @@ fs.readdirSync('controllers').forEach(function (file) {
         route.controller(app)
     }
 })
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-	extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // catch 404 and forward to error handler
