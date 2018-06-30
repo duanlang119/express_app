300810e6b22cf7fc6b67d40b6623d876dc44353f
Guoxing Yao <guoxing.yao@ericsson.com>
2018-06-30 20:56:01 +0800

    chapter 5-2 simple vuetify app.

--- movie_rating_app/src/router/index.js
+++ movie_rating_app/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Contact from '@/components/Contact';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home,
+    },{
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
+    },
   ]
 })
